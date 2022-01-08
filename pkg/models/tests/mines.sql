d637713d7f53cff81c8b1674a8787688d11f9b40
Sean Egan <segan@cm-logic.com>
2022-01-08 16:29:10 +0000

    added more test data

--- pkg/models/tests/mines.sql
+++ pkg/models/tests/mines.sql
@@ -4,5 +4,7 @@ CREATE TABLE mines (
                        name TEXT  DEFAULT '',
                        purl_type  TEXT  DEFAULT ''
 );
-INSERT INTO mines (id, name, purl_type) VALUES (0, 'maven.org', 'maven');
-INSERT INTO mines (id, name, purl_type) VALUES (1, 'rubygems.org', 'gem');
+INSERT INTO mines (id, name, purl_type) VALUES (0,  'maven.org',    'maven');
+INSERT INTO mines (id, name, purl_type) VALUES (1,  'rubygems.org', 'gem');
+INSERT INTO mines (id, name, purl_type) VALUES (2,  'npmjs.org',    'npm');
+-- INSERT INTO mines (id, name, purl_type) VALUES (27, 'nodejs.org',   'npm');

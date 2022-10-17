93f4aa6f359ac0195cec0feb446d39777e0a8893
Sean Egan <segan@cm-logic.com>
2022-10-17 19:23:52 +0100

    added logging and git_license_id support

--- pkg/models/projects.go
+++ pkg/models/projects.go
@@ -23,47 +23,54 @@ import (
 	"errors"
 	"fmt"
 	"github.com/jmoiron/sqlx"
-	zlog "scanoss.com/dependencies/pkg/logger"
+	"go.uber.org/zap"
 )
 
 type projectModel struct {
 	ctx  context.Context
+	s    *zap.SugaredLogger
 	conn *sqlx.Conn
 }
 
 type Project struct {
-	Component string `db:"component"`
-	License   string `db:"license"`
-	LicenseId string `db:"license_id"`
-	IsSpdx    bool   `db:"is_spdx"`
-	PurlName  string `db:"purl_name"`
+	PurlName     string `db:"purl_name"`
+	Component    string `db:"component"`
+	License      string `db:"license"`
+	LicenseId    string `db:"license_id"`
+	IsSpdx       bool   `db:"is_spdx"`
+	GitLicense   string `db:"g_license"`
+	GitLicenseId string `db:"g_license_id"`
+	GitIsSpdx    bool   `db:"g_is_spdx"`
 }
 
 // NewProjectModel creates a new instance of the Project Model
-func NewProjectModel(ctx context.Context, conn *sqlx.Conn) *projectModel {
-	return &projectModel{ctx: ctx, conn: conn}
+func NewProjectModel(ctx context.Context, s *zap.SugaredLogger, conn *sqlx.Conn) *projectModel {
+	return &projectModel{ctx: ctx, s: s, conn: conn}
 }
 
 // GetProjectsByPurlName searches the projects' table for details about Purl Name and Type
 func (m *projectModel) GetProjectsByPurlName(purlName string, purlType string) ([]Project, error) {
 	if len(purlName) == 0 {
-		zlog.S.Error("Please specify a valid Purl Name to query")
+		m.s.Error("Please specify a valid Purl Name to query")
 		return nil, errors.New("please specify a valid Purl Name to query")
 	}
 	if len(purlType) == 0 {
-		zlog.S.Error("Please specify a valid Purl Type to query")
+		m.s.Error("Please specify a valid Purl Type to query")
 		return nil, errors.New("please specify a valid Purl Type to query")
 	}
 	var allProjects []Project
 	err := m.conn.SelectContext(m.ctx, &allProjects,
 		"SELECT purl_name, component,"+
-			" l.license_name AS license, l.spdx_id AS license_id, l.is_spdx AS is_spdx FROM projects p"+
+			" l.license_name AS   license, l.spdx_id AS   license_id, l.is_spdx AS   is_spdx,"+
+			" g.license_name AS g_license, g.spdx_id AS g_license_id, g.is_spdx AS g_is_spdx"+
+			" FROM projects p"+
 			" LEFT JOIN mines m ON p.mine_id = m.id"+
 			" LEFT JOIN licenses l ON p.license_id = l.id"+
+			" LEFT JOIN licenses g ON p.git_license_id = g.id"+
 			" WHERE m.purl_type = $1 AND p.purl_name = $2",
 		purlType, purlName)
 	if err != nil {
-		zlog.S.Errorf("Error: Failed to query projects table for %v, %v: %v", purlName, purlType, err)
+		m.s.Errorf("Failed to query projects table for %v, %v: %v", purlName, purlType, err)
 		return nil, fmt.Errorf("failed to query the projects table: %v", err)
 	}
 	return allProjects, nil
@@ -72,29 +79,33 @@ func (m *projectModel) GetProjectsByPurlName(purlName string, purlType string) (
 // GetProjectByPurlName searches the projects' table for details about a Purl Name and Mine ID
 func (m *projectModel) GetProjectByPurlName(purlName string, mineId int32) (Project, error) {
 	if len(purlName) == 0 {
-		zlog.S.Error("Please specify a valid Purl Name to query")
+		m.s.Error("Please specify a valid Purl Name to query")
 		return Project{}, errors.New("please specify a valid Purl Name to query")
 	}
 	if mineId < 0 {
-		zlog.S.Error("Please specify a valid Mine ID to query")
+		m.s.Error("Please specify a valid Mine ID to query")
 		return Project{}, errors.New("please specify a valid Mine ID to query")
 	}
 	rows, err := m.conn.QueryxContext(m.ctx,
-		"SELECT purl_name, component, l.license_name AS license, l.spdx_id AS license_id, l.is_spdx AS is_spdx FROM projects p"+
+		"SELECT purl_name, component,"+
+			" l.license_name AS   license, l.spdx_id AS   license_id, l.is_spdx AS   is_spdx,"+
+			" g.license_name AS g_license, g.spdx_id AS g_license_id, g.is_spdx AS g_is_spdx"+
+			" FROM projects p"+
 			" LEFT JOIN licenses l ON p.license_id = l.id"+
+			" LEFT JOIN licenses g ON p.git_license_id = g.id"+
 			" WHERE purl_name = $1 AND mine_id = $2",
 		purlName, mineId)
 	defer CloseRows(rows)
 	if err != nil {
-		zlog.S.Errorf("Error: Failed to query projects table for %v, %v: %v", purlName, mineId, err)
+		m.s.Errorf("Error: Failed to query projects table for %v, %v: %v", purlName, mineId, err)
 		return Project{}, fmt.Errorf("failed to query the projects table: %v", err)
 	}
 	var project Project
 	if rows.Next() {
 		err = rows.StructScan(&project)
 		if err != nil {
-			zlog.S.Errorf("Failed to parse projects table results for %#v: %v", rows, err)
-			zlog.S.Errorf("Query failed for purl_name = %v, mine_id = %v", purlName, mineId)
+			m.s.Errorf("Failed to parse projects table results for %#v: %v", rows, err)
+			m.s.Errorf("Query failed for purl_name = %v, mine_id = %v", purlName, mineId)
 			return Project{}, fmt.Errorf("failed to query the projects table: %v", err)
 		}
 	}
